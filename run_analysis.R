482eae6b919c0dcfefd4bc75f9bbc1e68134676e
BHAUTIK A. MANGUKIYA <104815590+bhautik-a-mangukiya@users.noreply.github.com>
2022-09-12 23:07:22 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,7 +5,7 @@ filename <- "Coursera_DS3_Final.zip"
 # Checking if archieve already exists.
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  download.file(fileURL, filename, method="curl")
+  download.file(fileURL, filename)
 }  
 
 # Checking if folder exists
